38d02c11d6d06dad85246ed017fa41baecf6e263
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2022-08-11 21:13:27 +0300

    Implemented Hello controller

---
+++ src/main/java/com/goit5/testapp/feature/HelloController.java
@@ -0,0 +1,14 @@
+package com.goit5.testapp.feature;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+}
